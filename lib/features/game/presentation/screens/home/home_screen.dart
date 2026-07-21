65a3e058b8ff2935d242dca132767c0ebedc6cb0
laele <luis.sorianocrespo@outlook.com>
2026-07-21 17:42:11 -0600

    correct effect added

--- lib/features/game/presentation/screens/home/home_screen.dart
+++ lib/features/game/presentation/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_confetti/flutter_confetti.dart';
 import 'package:flutter_math_app/features/audio/presentation/cubit/audio_cubit.dart';
 import 'package:flutter_math_app/features/game/domain/entities/game_sound_event.dart';
 import 'package:flutter_math_app/features/game/presentation/game_cubit/game_cubit.dart';
@@ -11,6 +14,33 @@ class HomeScreen extends StatelessWidget {
   final GlobalKey<HomePlayCanvasState> homePlayCanvasKey = GlobalKey<HomePlayCanvasState>();
 
   HomeScreen({super.key});
+
+  void playEffect(BuildContext context) {
+    const options = ConfettiOptions(
+      spread: 360,
+      ticks: 50,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+      colors: [Color(0xffFFE400), Color(0xffFFBD00), Color(0xffE89400), Color(0xffFFCA6C), Color(0xffFDFFB8)],
+    );
+
+    shoot() {
+      Confetti.launch(context, options: options.copyWith(particleCount: 20, scalar: 1.7), particleBuilder: (index) => Star());
+      Confetti.launch(
+        context,
+        options: options.copyWith(
+          particleCount: 10,
+          scalar: 0.65,
+        ),
+        particleBuilder: (index) => Star(),
+      );
+    }
+
+    Timer(Duration.zero, shoot);
+    Timer(const Duration(milliseconds: 100), shoot);
+    //Timer(const Duration(milliseconds: 200), shoot);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +57,7 @@ class HomeScreen extends StatelessWidget {
         final audioCubit = context.read<AudioCubit>();
         switch (state.soundEvent!.type) {
           case GameSoundType.correct:
+            playEffect(context);
             audioCubit.playSfxCorrect();
           case GameSoundType.incorrect:
             audioCubit.playSfxIncorrect();
---
+++ lib/features/game/presentation/screens/home/widgets/home_effects.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameEffects extends StatelessWidget {
+  const GameEffects({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
