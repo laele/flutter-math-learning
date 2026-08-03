1e3c2687912950b40bb6a1dbf58260e715433464
laele <luis.sorianocrespo@outlook.com>
2026-08-03 17:41:39 -0600

    shake effect added scale

---
+++ lib/features/game/score/widgets/score_badge.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ScoreBadge extends StatelessWidget {
+  final double widthSize;
+  final double heightSize;
+  final Widget child;
+  const ScoreBadge({super.key, required this.widthSize, required this.heightSize, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widthSize,
+      height: heightSize,
+      clipBehavior: Clip.none,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Center(
+            child: Image.asset(
+              'lib/core/assets/images/star.png',
+              color: Colors.white,
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: child,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
