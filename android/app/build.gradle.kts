5802fc8c06df179bf8542de6ae87e182b46aeac7
laele <luis.sorianocrespo@outlook.com>
2026-08-25 19:17:27 -0600

    added admob app id

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders += [
+        ADMOB_APP_ID: project.findProperty('ADMOB_APP_ID') ?: "ca-app-pub-3940256099942544~3347511713" // Fallback al id de prueba si no existe
+    ]
     }
 
     buildTypes {
